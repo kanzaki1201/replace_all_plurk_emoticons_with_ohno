ff091953c22775af07ddc2afb386729a04193582
kanzaki1201 <karma_824@hotmail.com>
2021-01-12 23:42:56 -0800

    Create replace_emoticon_with_ohno.js

---
+++ replace_emoticon_with_ohno.js
@@ -0,0 +1,20 @@
+// ==UserScript==
+// @name         替換所有噗浪表符成大野智拍手笑
+// @version      0.1
+// @description  將所有噗浪表符換成大野智拍手笑，當你很想念韓國阿姨的時候可以用
+// @author       Kanzaki K
+// @match        https://www.plurk.com/*
+// @require      https://code.jquery.com/jquery-3.5.1.slim.js
+// ==/UserScript==
+
+// Your code here...
+
+'use strict';
+
+$(document).ready(replace_with_ohno);
+
+function replace_with_ohno(){
+	setInterval( () => {
+		$(".emoticon_my").attr("src", "https://emos.plurk.com/8e67570973b3ff9a77888a773d974fec_w48_h48.gif");
+	}, 5000);
+}
